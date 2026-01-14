5eec1da5cbec8aaf875f03c69cc2b394c89b8993
anindasadhukhan-hackclub <indiansnacksbusiness@gmail.com>
2026-01-14 22:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import scientific_calculator as calc
+import os
 
 app = Flask(__name__)
 
@@ -69,5 +70,8 @@ def home():
 
     return render_template("index.html", result=result)
 
+
+# ✅ REQUIRED FOR RENDER
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
